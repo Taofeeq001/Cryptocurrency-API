50623d0eb2c4e484291639dc561bc8cfa9ae80d9
Taofeeq001 <taofeeqkolapo@gmail.com>
2023-06-02 05:22:33 -0400

    Updating the crytpo table

--- src/Components/page/Front.jsx
+++ src/Components/page/Front.jsx
@@ -10,7 +10,7 @@ const Front = () => {
     const base_url =
       "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
     try {
-      const response = await fetch(base_url)
+      const response = await fetch(base_url, {mode:"cors"})
         .then((res) => res.json())
         .catch((error) => console.log(error));
       setCoin(response);
@@ -42,7 +42,7 @@ const Front = () => {
           placeholder="Input your coin here"
         />
       </form>
-      <div>
+      <div className="coinss">
         {filteredCoins.map((coin) => (
           <CoinDetails
             key={coin.id}
